703270700d4200120f06253a738b86f2b31ee9c0
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-08-29 19:16:18 -0300

    feature: criando estrutura db dados

--- Blockchain/blockchain.go
+++ Blockchain/blockchain.go
@@ -1,11 +1,59 @@
 package blockchain
 
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
 type Blockchain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
 func InitBlockChain() *Blockchain {
-	return &Blockchain{[]*Block{Genesis()}}
+	var lastHash []byte
+	opts := badger.DefaultOptions(dbPath)
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("Blockchain não encontrado. Criando um novo...")
+
+			genesis := Genesis()
+			println("Genesis criado")
+
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+
+			err = txn.Set([]byte("lh"), genesis.Hash)
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+
+			err1 := item.Value(func(val []byte) error {
+				lastHash = append([]byte{}, val...)
+				return nil
+			})
+			Handle(err1)
+
+			return err
+		}
+	})
+	Handle(err)
+	blockchain := Blockchain{lastHash, db}
+	return &blockchain
 }
 
 func (chain *Blockchain) AddBlock(data string) {
