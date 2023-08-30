d63852efdd1934ab9d144ccbbdf286fc957f64e7
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-08-30 00:31:40 -0300

    feature: refatorando a class main.go

--- Blockchain/blockchain.go
+++ Blockchain/blockchain.go
@@ -15,6 +15,11 @@ type Blockchain struct {
 	Database *badger.DB
 }
 
+type BlockchainIterator struct {
+	CurrentHash []byte
+	Database    *badger.DB
+}
+
 func InitBlockChain() *Blockchain {
 	var lastHash []byte
 	opts := badger.DefaultOptions(dbPath)
@@ -57,7 +62,64 @@ func InitBlockChain() *Blockchain {
 }
 
 func (chain *Blockchain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
+	var lastHash []byte
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+
+		err1 := item.Value(func(val []byte) error {
+			lastHash = append([]byte{}, val...)
+			return nil
+		})
+
+		return err1
+	})
+
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+		return err
+	})
+
+	Handle(err)
+}
+
+func (chain *Blockchain) Iterator() *BlockchainIterator {
+	iter := &BlockchainIterator{chain.LastHash, chain.Database}
+
+	return iter
+}
+
+func (iter *BlockchainIterator) Next() *Block {
+	var block *Block
+	var blockData []byte
+
+	err := iter.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iter.CurrentHash)
+		Handle(err)
+
+		err1 := item.Value(func(val []byte) error {
+			blockData = append([]byte{}, val...)
+			return nil
+		})
+
+		Handle(err1)
+
+		block = Deserialize(blockData)
+		return err
+	})
+
+	Handle(err)
+
+	iter.CurrentHash = block.PrevHash
+
+	return block
 }
--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"runtime"
 	"strconv"
 
 	blockchain "github.com/tensor-programming/golang-blockchain/Blockchain"
 )
 
-func main() {
-	chain := blockchain.InitBlockChain()
-	chain.AddBlock("Primeiro bloco depois do Gênesis!")
-	chain.AddBlock("Segundo bloco depois do Gênesis!")
-	chain.AddBlock("Terceiro bloco depois do Gênesis!")
+type CommandLine struct {
+	blockchain *blockchain.Blockchain
+}
 
-	for _, block := range chain.Blocks {
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage: ")
+	fmt.Println("  add -block BLOCK_DATA - adiciona um bloco à blockchain")
+	fmt.Println("  print - imprime todos os blocos da blockchain")
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		{
+			cli.printUsage()
+			runtime.Goexit()
+		}
+	}
+}
+
+func (cli *CommandLine) addBlock(data string) {
+	cli.blockchain.AddBlock(data)
+	fmt.Println("Bloco adicionado!")
+}
+
+func (cli *CommandLine) printChain() {
+	iter := cli.blockchain.Iterator()
+
+	for {
+		block := iter.Next()
+
 		fmt.Printf("Hash anterior: %x\n", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
@@ -21,5 +47,54 @@ func main() {
 		pow := blockchain.NewProof(block)
 		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
 	}
+}
+
+func (cli *CommandLine) run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("block", "", "Bloco a ser adicionado")
+
+	switch os.Args[1] {
+	case "add":
+		err := addBlockCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+
+	case "print":
+		err := printChainCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func main() {
+	defer os.Exit(0)
+	chain := blockchain.InitBlockChain()
+
+	defer chain.Database.Close()
+
+	cli := CommandLine{chain}
+	cli.run()
 }
